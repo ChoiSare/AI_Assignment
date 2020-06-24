15d1df350d30063ed4fa069142331b3e6da54877
ChoiSare <66123469+ChoiSare@users.noreply.github.com>
2020-06-24 21:53:56 +0900

    Add files via upload

---
+++ 3주차 과제/3주차 과제.py
@@ -0,0 +1,9 @@
+num1 = int(input('첫번째 값을 입력하시오:'))
+num2 = int(input('두번째 값을 입력하시오:'))
+
+print('num1 + num2 =', num1 + num2)
+print('num1 - num2 =', num1 - num2)
+print('num1 * num2 =', num1 * num2)
+print('num1 / num2 =', num1 / num2)
+print('num1 // num2 =', num1 // num2)
+print('num1 % num2 =', num1 % num2)
